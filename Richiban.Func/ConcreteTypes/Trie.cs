f28797b20520f4c4ef1f8915ec019e1226118ce8
Richard Gibson <richard@gibson.cx>
2017-11-14 14:12:09 +0000

    Added some instructional comments

--- Richiban.Func/ConcreteTypes/Trie.cs
+++ Richiban.Func/ConcreteTypes/Trie.cs
@@ -28,7 +28,7 @@ namespace Richiban.Func
             set => Set(new StringSlice(key), value);
         }
 
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        public IEnumerator<T> GetEnumerator()
         {
             var results = new List<T>();
 
@@ -37,8 +37,9 @@ namespace Richiban.Func
             return results.GetEnumerator();
         }
 
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<T>) this).GetEnumerator();
-        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public Option<T> TryGet(string key) => Get(new StringSlice(key));
         public void Remove(string key) => Set(new StringSlice(key), Option<T>.None);
 
         private void EnumerateInto(ICollection<T> result)
@@ -52,11 +53,23 @@ namespace Richiban.Func
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns>
+        /// True if the value was set either in this node or one of its subtries. False otherwise
+        /// </returns>
         private bool Set(StringSlice key, Option<T> value)
         {
+            // If the current char in the key is not our node char then we're going down the wrong
+            // branch and we can quit early.
             if (key.Current != _nodeChar)
                 return false;
 
+            // If we have reached the end of the key then we are at the correct node at which to actually
+            // set the value
             if (key.AtEnd)
             {
                 _value = value;
@@ -64,44 +77,45 @@ namespace Richiban.Func
                 return true;
             }
 
+            // By this point we are on the correct branch of the trie but still have further to go.
+
+            // Work out what the next key is
             var nextKey = key.Next();
+
+            // Ask our subtries if they are able to set the value (i.e. the requisite subtrie is extant)
             var addedToSubTrie = _subTries.Any(t => t.Set(nextKey, value));
 
+            // If the value was added to any of the subtries then we're all done and can exit
             if (addedToSubTrie)
                 return true;
 
+            // If the value being set is None then we don't actually need to do anything; the value we are
+            // removing is already non-existent
             if (value.IsSome == false)
-            {
                 return true;
-            }
 
+            // We have reached the end of the current branch and now need to create a new subtrie to
+            // extend the branch
             var newSubTrie = new Trie<T>(nextKey.Current);
             _subTries.Add(newSubTrie);
 
+            // Now add the value to the new subtrie
             return newSubTrie.Set(nextKey, value);
         }
-
-        public Option<T> TryGet(string key) => Get(new StringSlice(key));
 
         private Option<T> Get(StringSlice key)
         {
             if (key.AtEnd)
             {
-                if (key.Current == _nodeChar && _value.IsSome)
-                {
+                if (key.Current == _nodeChar)
                     return _value;
-                }
-                else
-                {
-                    return Option<T>.None;
-                }
-            }
-            else
-            {
-                var nextSlice = key.Next();
 
-                return _subTries.Select(trie => trie.Get(nextSlice)).FirstOrDefault(x => x.IsSome);
+                return Option<T>.None;
             }
+
+            var nextSlice = key.Next();
+
+            return _subTries.Select(trie => trie.Get(nextSlice)).FirstOrDefault(x => x.IsSome);
         }
     }
 }
